b32bbe741c77651908d9c79aba93eec127b3bb6c
Benjamin Ricchiuto <ricchben@students.zhaw.ch>
2019-04-10 08:31:56 +0200

    Fixed directory bug

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export * from './HTTPServer';
 export * from './HTTPSServer';
 export * from './websocket/index';
 export * from './enum/index';
-export * from './data_container/index';
+export * from './repository/index';
 export * from './controller/index';
