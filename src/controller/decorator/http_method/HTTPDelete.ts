64eb37fec884441550b802c859b67e6ddd92bac9
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:49 +0200

    Delete HTTPDelete.ts

--- src/controller/decorator/http_method/HTTPDelete.ts
+++
@@ -1,6 +0,0 @@
-import { HttpDeleteAction } from '../../helper/http_method/HttpDeleteAction';
-import { assign } from './assign';
-
-export function HttpDelete(path: string): Function {
-    return assign(path, HttpDeleteAction);
-}
