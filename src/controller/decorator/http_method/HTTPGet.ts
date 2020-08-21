fccabab3c4520eab06511d0a00e4399f57b8f36c
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:42 +0200

    Delete HTTPGet.ts

--- src/controller/decorator/http_method/HTTPGet.ts
+++
@@ -1,6 +0,0 @@
-import { HttpGetAction } from '../../helper/http_method/HttpGetAction';
-import { assign } from './assign';
-
-export function HttpGet(path: string): Function {
-    return assign(path, HttpGetAction);
-}
