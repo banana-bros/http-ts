e2bc615168df2649c101ab90e70a7dbedb9caffc
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:19 +0200

    Delete HTTPPut.ts

--- src/controller/decorator/http_method/HTTPPut.ts
+++
@@ -1,6 +0,0 @@
-import { HttpPutAction } from '../../helper/http_method/HttpPutAction';
-import { assign } from './assign';
-
-export function HttpPut(path: string): Function {
-    return assign(path, HttpPutAction);
-}
