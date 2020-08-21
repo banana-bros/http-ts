5f124068dc99e0d5e93e9823fe4b6f248742d934
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:37 +0200

    Delete HTTPHead.ts

--- src/controller/decorator/http_method/HTTPHead.ts
+++
@@ -1,6 +0,0 @@
-import { HttpHeadAction } from '../../helper/http_method/HttpHeadAction';
-import { assign } from './assign';
-
-export function HttpHead(path: string): Function {
-    return assign(path, HttpHeadAction);
-}
