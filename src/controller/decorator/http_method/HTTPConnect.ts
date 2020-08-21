9a9f6d837f369e906f9ccf993ce471c5f92c3232
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:54 +0200

    Delete HTTPConnect.ts

--- src/controller/decorator/http_method/HTTPConnect.ts
+++
@@ -1,6 +0,0 @@
-import { HttpConnectAction } from '../../helper/http_method/HttpConnectAction';
-import { assign } from './assign';
-
-export function HttpConnect(path: string): Function {
-    return assign(path, HttpConnectAction);
-}
