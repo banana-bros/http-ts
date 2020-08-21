5c236b89e7c7c514560660efaab9fb4974698268
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:25:18 +0200

    Delete HTTPForbiddenError.ts

--- src/error/http-error/HTTPForbiddenError.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from './HttpError';
-
-export class HttpForbiddenError extends HttpError {
-    constructor(message: string = null) {
-        super(403, 'Forbidden', message);
-    }
-}
