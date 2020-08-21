fc8131a56f17b9d913f05452c4c87601da3d7e12
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:31 +0200

    Delete HTTPPatch.ts

--- src/controller/decorator/http_method/HTTPPatch.ts
+++
@@ -1,6 +0,0 @@
-import { HttpPatchAction } from '../../helper/http_method/HttpPatchAction';
-import { assign } from './assign';
-
-export function HttpPatch(path: string): Function {
-    return assign(path, HttpPatchAction);
-}
