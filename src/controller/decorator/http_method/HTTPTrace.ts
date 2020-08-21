f716a0e453733d492f65436f381f793413309e42
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:13 +0200

    Delete HTTPTrace.ts

--- src/controller/decorator/http_method/HTTPTrace.ts
+++
@@ -1,6 +0,0 @@
-import { HttpTraceAction } from '../../helper/http_method/HttpTraceAction';
-import { assign } from './assign';
-
-export function HttpTrace(path: string): Function {
-    return assign(path, HttpTraceAction);
-}
