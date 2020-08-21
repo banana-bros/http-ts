c54b9a332e3dca4c73b4344b6c7543c9bb2acb84
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:25:32 +0200

    Delete HTTPUnauthorizedError.ts

--- src/error/http-error/HTTPUnauthorizedError.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from './HttpError';
-
-export class HttpUnauthorizedError extends HttpError {
-    constructor(message: string = null) {
-        super(401, 'Unauthorized', message);
-    }
-}
