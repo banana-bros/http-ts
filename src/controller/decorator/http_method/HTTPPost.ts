ca34032753a12d876ac9a04def55a8054e3c7146
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2020-08-21 10:26:25 +0200

    Delete HTTPPost.ts

--- src/controller/decorator/http_method/HTTPPost.ts
+++
@@ -1,6 +0,0 @@
-import { HttpPostAction } from '../../helper/http_method/HttpPostAction';
-import { assign } from './assign';
-
-export function HttpPost(path: string): Function {
-    return assign(path, HttpPostAction);
-}
